08abdbd8447ba7b1c95cacd17f2ffeaa407c3b74
amithhhh <amithajith7025@gmail.com>
2024-03-12 18:28:45 +0000

    created template inheritance

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Home page")
+    return render(request,'home.html')
 
 def room(request):
-    return HttpResponse("Room Page")
+    return render(request,'room.html')
